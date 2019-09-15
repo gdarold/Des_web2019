99fc79bfa656ea08412378fff5dc7507c7849642
Gabriel Darold <41766961+gdarold@users.noreply.github.com>
2019-09-14 22:35:43 -0300

    Create testa_carro.py

---
+++ testa_carro.py
@@ -0,0 +1,19 @@
+from carro import Carro
+
+
+
+class teste:
+
+
+	brasilia = Carro(8)
+	brasilia.adicionar_gasolina(50)
+	
+	brasilia.andar(380)
+
+	brasilia.andar(380)
+	brasilia.obter_galolina()
+	brasilia.adicionar_gasolina(45)
+
+	
+
+
